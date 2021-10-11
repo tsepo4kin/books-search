5990a3b41be9f56da5f038a2705fb831ae034e26
Kirill <kupuk132@gmail.com>
2021-10-11 11:29:55 +0300

    feat: implement tests for searchReducer

--- src/store/__tests__/searchReducer.test.js
+++ src/store/__tests__/searchReducer.test.js
@@ -1,7 +1,15 @@
 import { searchReducer } from "../searchReducer"; 
+import { changeSearchParams } from "../actions"
 
-describe('', () => {
-  it('', () => {
-    
+describe('testing search reducer', () => {
+  it('shoud change search params', () => {
+    const newSearchParams = {inputValue: 'testValue', categoriesValue: 'testCategories', sortingValue: 'testSorting'}
+    const changeSearchParamsAction = changeSearchParams(newSearchParams);
+
+    const state = {};
+
+    const newState = searchReducer(state, changeSearchParamsAction);
+
+    expect(newState).toEqual(newSearchParams)
   })
 })
