590e17d57167a6dd46e5af41fae2744673020ce4
Kirill <kupuk132@gmail.com>
2021-10-11 11:21:04 +0300

    feat: implement tests for loaderReducer

--- src/store/__tests__/loaderReducer.test.js
+++ src/store/__tests__/loaderReducer.test.js
@@ -1,7 +1,40 @@
 import { loaderReducer } from "../loaderReducer"; 
+import { toggleSearchLoader, togglePaginationLoader } from "../actions"
 
-describe('', () => {
-  it('', () => {
-    
+describe('testing loader reducer', () => {
+  it('search loader shoud enable', () => {
+    const toggleSearchLoaderAction = toggleSearchLoader()
+    const state = {searchLoader: false, paginationLoader: false}
+
+    const newState = loaderReducer(state, toggleSearchLoaderAction)
+
+    expect(newState.searchLoader).toBe(true)
+  })
+
+  it('search loader shoud disable', () => {
+    const toggleSearchLoaderAction = toggleSearchLoader()
+    const state = {searchLoader: true, paginationLoader: false}
+
+    const newState = loaderReducer(state, toggleSearchLoaderAction)
+
+    expect(newState.searchLoader).toBe(false)
+  })
+
+  it('pagination loader shoud enable', () => {
+    const togglePaginationLoaderAction = togglePaginationLoader()
+    const state = {searchLoader: false, paginationLoader: false}
+
+    const newState = loaderReducer(state, togglePaginationLoaderAction)
+
+    expect(newState.paginationLoader).toBe(true)
+  })
+
+  it('pagination loader shoud disable', () => {
+    const togglePaginationLoaderAction = togglePaginationLoader()
+    const state = {searchLoader: false, paginationLoader: true}
+
+    const newState = loaderReducer(state, togglePaginationLoaderAction)
+
+    expect(newState.paginationLoader).toBe(false)
   })
 })
